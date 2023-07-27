7e0c40a2e9e32174c8c6df51c2ce524109381e57
husush23 <husseinkadare40@gmail.com>
2023-07-27 11:13:17 +0300

    Decode word and decode char written

---
+++ decode.rb
@@ -0,0 +1,39 @@
+def decode_char(morse_code)
+    morse_to_char = {
+      ".-" => "A",
+        "-..." => "B",
+        "-.-." => "C",
+        "-.." => "D",
+        "." => "E",
+        "..-." => "F",
+        "--." => "G",
+        "...." => "H",
+        ".." => "I",
+        ".---" => "J",
+        "-.-" => "K",
+        ".-.." => "L",
+        "--" => "M",
+        "-." => "N",
+        "---" => "O",
+        ".--." => "P",
+        "--.-" => "Q",
+        ".-." => "R",
+        "..." => "S",
+        "-" => "T",
+        "..-" => "U",
+        "...-" => "V",
+        ".--" => "W",
+        "-..-" => "X",
+        "-.--" => "Y",
+        "--.." => "Z"
+      }
+      
+     return  morse_to_char[morse_code]
+    end
+    
+    
+    
+     def decode_word(morse_code)
+     morse_code.split.map{ |morse| decode_char(morse)}.join
+    
+    end
